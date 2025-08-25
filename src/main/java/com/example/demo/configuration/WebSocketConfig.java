115d647b4fecd7bf1f454a2e8376aea87f123b8d
jiwon <jwp0329@naver.com>
2025-08-25 09:19:17 +0900

    FEAT: WebSocketHandler 주입
    주입된 웹 소켓 핸들러 객체로 registry 추가

--- src/main/java/com/example/demo/configuration/WebSocketConfig.java
+++ src/main/java/com/example/demo/configuration/WebSocketConfig.java
@@ -24,12 +24,10 @@ public class WebSocketConfig implements WebSocketConfigurer {
         registry
                 // 클라이언트에서 웹 소켓 연결을 위해 "ws-stomp"라는 엔드포인트로 연결을 시도하면
                 // ChatWebSocketHandler 클래스에서 이를 처리합니다.
-                .addHandler(new ChatWebSocketHandler(), "/ws-stomp")
+                .addHandler(chatWebSocketHandler, "/ws-stomp")
                 // 접속 시도하는 모든 도메인 또는 IP에서 WebSocket 연결을 허용합니다.
                 .setAllowedOrigins("*")
                 // SockJS를 사용하여 WebSocket 연결을 지원하지 않는 클라이언트에서도 연결할 수 있도록 합니다.
                 .withSockJS();
     }
-
-
 }
